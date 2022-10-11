915fb6108fd097172a0fb08c4ed002160d4e36b9
Jaskaran Singh <jsingh2250@gmail.com>
2022-10-11 01:56:57 -0400

    Made the calculator faster by storing numbers in the sequence to reduce the number of recursive calls that need to be made.

--- src/FibonacciNumber.java
+++ src/FibonacciNumber.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+
 /** This class can be used to calculate the nth number in the Fibonacci Sequence.
  * 
  * @author Jaskaran Singh
  */
 public class FibonacciNumber {
+    // An array list for storing numbers in the Fibonacci Sequence to allow for quicker calculation of the nth Fibonacci number.
+    private static ArrayList<Integer> fibonacciSequence = new ArrayList<>();
     /**
      * Calculates the specified nth number in the Fibonacci Sequence. Returns the specified nth number in the Fibonacci Sequence.
      * 
@@ -14,8 +18,16 @@ public class FibonacciNumber {
         if (n == 0 || n == 1) {
             return n;
         }
+        // Get the number from the array list if it's already been stored.
+        else if ((n - 2) < fibonacciSequence.size()) {
+            return fibonacciSequence.get(n - 2);
+        }
+        // Recursively calculate and store the nth Fibonacci number.
+        else {
+            fibonacciSequence.add(getNthFibonacciNumber(n - 1) + getNthFibonacciNumber(n - 2));
 
-        // Recursively calculate the nth Fibonacci number.
-        return getNthFibonacciNumber(n - 1) + getNthFibonacciNumber(n - 2);
+            // Return the specified nth number in the Fibonacci Sequence.
+            return fibonacciSequence.get(n - 2);
+        }
     }
 }
